2cb47823a7ea41e27cd15adc1d58a5d9f3fdee12
Ayush <ayushsinha4343@gmail.com>
2026-02-13 18:19:56 +0530

    first commit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,25 +4,25 @@ import { supabase } from "@/lib/supabaseClient";
 import { FaGoogle } from "react-icons/fa";
 
 export default function LoginPage() {
-
   const signIn = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "https://smart-bookmarks-app-three.vercel.app/",
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       },
     });
 
-    if (error) console.error(error);
+    if (error) console.error("Google login error:", error.message);
   };
 
   return (
-    <div className="flex justify-center items-center bg-neutral-500 h-screen">
+    <div className="flex justify-center items-center bg-neutral-900 h-screen">
       <button 
         onClick={signIn}
-        className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white text-xl rounded flex items-center gap-2 "
+        className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white text-xl rounded-lg flex items-center gap-3 shadow-lg"
       >
-        Sign in with Google <FaGoogle className="text-red-600" />
+        <FaGoogle className="text-white text-2xl" />
+        Sign in with Google
       </button>
     </div>
   );
